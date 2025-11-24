b7cf6d82941364cd40740ef3cddce1002a6f4307
Muhammad Danial <61508987+danial798@users.noreply.github.com>
2025-11-24 17:34:38 +0500

    b

--- read_pdf.py
+++
@@ -1,9 +0,0 @@
-from pypdf import PdfReader
-
-reader = PdfReader("ML_Portfolio_Project (1).pdf")
-text = ""
-# Read first 3 pages
-for i in range(min(3, len(reader.pages))):
-    text += reader.pages[i].extract_text() + "\n"
-
-print(text)
